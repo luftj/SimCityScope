d81c0913144e3a88d29b250dcc3e4f7fc4147d2e
Jonas <jonas.luft@hcu-hamburg.de>
2019-04-03 13:23:33 +0200

    refactor, readme

---
+++ Interface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimCityScope
+{
+    delegate void InterfaceDelegate();
+
+    class InterfaceElement
+    {
+        public static int width = 50;
+        public static int height = 50;
+        public string name { get; }
+        public InterfaceDelegate action;
+
+        public InterfaceElement(string name, InterfaceDelegate action)
+        {
+            this.name = name;
+            this.action = action;
+        }
+    }
+
+    enum InterfaceState
+    {
+        NONE = 0,
+        ROAD,
+        COMM,
+        RES,
+
+        NUM_INTERFACESTATE
+    }
+
+    class Interface
+    {
+    }
+}
---
+++ todo.md
@@ -0,0 +1,16 @@
+# TO DO
+
+General
+- [ ] write readme
+
+View
+- [ ] ...
+
+Control
+- [ ] rectangle zoning
+  - [ ] mouse
+  - [ ] touch
+
+
+Model
+- [ ] ...
